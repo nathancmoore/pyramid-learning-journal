858302eb9e5e998d8b68db8946795eeeee96a4f4
Nathan Moore <ncmoore1986@gmail.com>
2017-11-05 22:32:02 -0800

    fixed my import of httpexception

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -2,7 +2,7 @@
 
 from pyramid.view import view_config
 from ..models.mymodel import MyModel
-from pyramid.HTTPException import HTTPFound, HTTPBadRequest
+from pyramid.httpexception import HTTPFound, HTTPBadRequest
 
 
 @view_config(route_name="home", renderer="pyramid_learning_journal:templates/index.jinja2")
