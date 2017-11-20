11950b317e12e851cabcbe41675b45fe34286bb7
Nathan Moore <ncmoore1986@gmail.com>
2017-11-20 08:34:00 -0800

    added login and logout views and imported needed stuff

--- pyramid_learning_journal/pyramid_learning_journal/views/default.py
+++ pyramid_learning_journal/pyramid_learning_journal/views/default.py
@@ -3,6 +3,8 @@
 from pyramid.view import view_config
 from ..models.mymodel import MyModel
 from pyramid.httpexceptions import HTTPFound, HTTPBadRequest
+from pyramid.security import remember, forget
+from pyramid_learning_journal.security import check_credentials
 
 
 @view_config(route_name="home", renderer="pyramid_learning_journal:templates/index.jinja2")
@@ -58,3 +60,24 @@ def update_view(request):
         request.dbsession.add(entry)
         request.dbsession.flush()
         return HTTPFound(request.route_url('details', id=entry.id))
+
+
+@view_config(route_name="login",
+             renderer="learning_journal:templates/login.jinja2",
+             require_csrf=False)
+def login_view(request):
+    """Function to return view for login page."""
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        if check_credentials(username, password):
+            headers = remember(request, username)
+            return HTTPFound(request.route_url('home'), headers=headers)
+    return {}
+
+
+@view_config(route_name='logout')
+def logout(request):
+    """Function to log a user out."""
+    headers = forget(request)
+    return HTTPFound(request.route_url('home'), headers=headers)
