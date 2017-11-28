49c206329e7d90e2a67ca4c19fc2a1008aed28db
Nathan Moore <ncmoore1986@gmail.com>
2017-11-28 08:41:20 -0800

    added some changes

--- pyramid_learning_journal/pyramid_learning_journal/__init__.py
+++ pyramid_learning_journal/pyramid_learning_journal/__init__.py
@@ -1,11 +1,13 @@
 """Define the main function, which kicks off this whole ball of wax."""
 
-
+import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
     """Start the app."""
+    if os.environ.get('DATABASE_URL', ''):
+        settings['sqlalchemy.url'] = os.environ['DATABASE_URL']
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.views')
